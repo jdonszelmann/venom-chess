7c5ac4aea05ce6fbbb6e68ec0641d64f5520cb48
jonay2000 <jonabent@gmail.com>
2021-03-05 22:00:31 +0100

    move extra

--- src/game_engine/chess_move.rs
+++ src/game_engine/chess_move.rs
@@ -1,3 +1,5 @@
+use crate::game_engine::piece::{Piece, knight_of_color, bishop_of_color, rook_of_color, queen_of_color};
+use crate::game_engine::color::Color;
 
 #[derive(Clone, Copy, Eq, PartialEq, Debug, Hash)]
 pub struct Location {
@@ -22,15 +24,99 @@ impl From<(i8, i8)> for Location {
     }
 }
 
+
+#[repr(u8)]
+enum Extra {
+    Quiet = 0,
+    DoublePawn = 1,
+    KingCastle = 2,
+    QueenCastle = 3,
+    Captures = 4,
+    EnPassantCaptures = 5,
+
+    KnightPromotion = 8,
+    BishopPromotion = 9,
+    RookPromotion = 10,
+    QueenPromotion = 11,
+
+    KnightPromotionCapture = 12,
+    BishopPromotionCapture = 13,
+    RookPromotionCapture = 14,
+    QueenPromotionCapture = 15,
+}
+
+impl Extra {
+    pub fn is_promotion(&self) -> bool {
+        self.is_capturing_promotion() || self.is_non_capturing_promotion()
+    }
+
+    pub fn is_non_capturing_promotion(&self) -> bool {
+        match self {
+            Extra::KnightPromotion => true,
+            Extra::BishopPromotion => true,
+            Extra::RookPromotion => true,
+            Extra::QueenPromotion => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_capturing_promotion(&self) -> bool {
+        match self {
+            Extra::KnightPromotionCapture => true,
+            Extra::BishopPromotionCapture => true,
+            Extra::RookPromotionCapture => true,
+            Extra::QueenPromotionCapture => true,
+            _ => false,
+        }
+    }
+
+    pub fn capturing_promotion(piece: Piece) -> Extra {
+        match piece {
+            Piece::Empty => panic!("can't promote empty"),
+            Piece::BlackPawn => panic!("can't promote pawn"),
+            Piece::WhitePawn => panic!("can't promote pawn"),
+            Piece::BlackBishop => Extra::BishopPromotion,
+            Piece::WhiteBishop => Extra::BishopPromotion,
+            Piece::BlackKnight => Extra::KnightPromotion,
+            Piece::WhiteKnight => Extra::KnightPromotion,
+            Piece::BlackRook => Extra::RookPromotion,
+            Piece::WhiteRook => Extra::RookPromotion,
+            Piece::BlackKing => panic!("can't promote king"),
+            Piece::WhiteKing => panic!("can't promote king"),
+            Piece::BlackQueen => Extra::QueenPromotion,
+            Piece::WhiteQueen => Extra::QueenPromotion,
+        }
+    }
+
+    pub fn promotion_of_color(&self, color: Color) -> Option<Piece> {
+        match self {
+            Extra::Quiet => None,
+            Extra::DoublePawn => None,
+            Extra::KingCastle => None,
+            Extra::QueenCastle => None,
+            Extra::Captures => None,
+            Extra::EnPassantCaptures => None,
+            Extra::KnightPromotion => Some(knight_of_color(color)),
+            Extra::BishopPromotion => Some(bishop_of_color(color)),
+            Extra::RookPromotion => Some(rook_of_color(color)),
+            Extra::QueenPromotion => Some(queen_of_color(color)),
+            Extra::KnightPromotionCapture => Some(knight_of_color(color)),
+            Extra::BishopPromotionCapture => Some(bishop_of_color(color)),
+            Extra::RookPromotionCapture => Some(rook_of_color(color)),
+            Extra::QueenPromotionCapture => Some(queen_of_color(color)),
+        }
+    }
+}
+
 #[derive(Clone, Copy, Eq, PartialEq, Debug, Hash)]
 pub struct Move {
     pub from: Location,
     pub to: Location,
-    pub extra: u8,
+    pub extra: Extra,
 }
 
 impl Move {
-    pub fn new(from: Location, to: Location, extra: u8) -> Self {
+    pub fn new(from: Location, to: Location, extra: Extra) -> Self {
         Self {
             from,
             to,
@@ -42,13 +128,13 @@ impl Move {
 impl From<(Location, Location)> for Move {
     fn from((from, to): (Location, Location)) -> Self {
         Self {
-            from, to, extra:0,
+            from, to, extra: Extra::Quiet,
         }
     }
 }
 
-impl From<(Location, Location, u8)> for Move {
-    fn from((from, to, extra): (Location, Location, u8)) -> Self {
+impl From<(Location, Location, Extra)> for Move {
+    fn from((from, to, extra): (Location, Location, Extra)) -> Self {
         Self {
             from, to, extra,
         }
@@ -65,8 +151,8 @@ impl From<((i8, i8), (i8, i8))> for Move {
     }
 }
 
-impl From<((i8, i8), (i8, i8), u8)> for Move {
-    fn from((from, to, extra): ((i8, i8), (i8, i8), u8)) -> Self {
+impl From<((i8, i8), (i8, i8), Extra)> for Move {
+    fn from((from, to, extra): ((i8, i8), (i8, i8), Extra)) -> Self {
         Self {
             from: from.into(),
             to: to.into(),
