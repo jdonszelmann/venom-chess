110e3a8b2f0e795ffbebdaf0c89c70f04eab0ae3
jonay2000 <jonabent@gmail.com>
2021-03-05 14:12:13 +0100

    added file I forgot

---
+++ src/game_engine/board/mod.rs
@@ -0,0 +1,27 @@
+use std::fmt;
+
+pub use basic::BasicBoard;
+
+use crate::game_engine::chess_move::{Location, Move};
+use crate::game_engine::color::Color;
+use crate::game_engine::piece::Piece;
+
+pub mod basic;
+
+pub trait Board: Sized {
+    fn moves(&self, location: impl Into<Location>) -> Vec<Move>;
+    fn all_moves(&self) -> Vec<Move>;
+    fn transition(&self, m: Move) -> Self;
+
+    fn all_pieces(&self) -> Vec<(Piece, Location)>;
+
+    fn is_terminal(&self) -> Option<Color>;
+
+
+    fn piece_at(&self, l: impl Into<Location>) -> Piece;
+    fn piece_at_mut(&mut self, l: impl Into<Location>) -> &mut Piece;
+
+
+
+}
+
